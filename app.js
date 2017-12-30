f6ef89430baeda8ded88d3be0ce477fed60f70d8
ThomasBerranger <tberranger@hotmail.fr>
2017-12-30 16:05:35 +0100

    Seconde partie

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var nunjucks = require('nunjucks');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+var upload = multer({
+    dest: __dirname + '/uploads'
+});
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1/pokedex');
@@ -10,10 +16,15 @@ require('./models/Type');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.single('file'));
+
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.use('/', require('./routes/pokemon'));
 app.use('/type', require('./routes/type'));
+
+app.use('/upload', express.static(__dirname + '/uploads'));
 
 nunjucks.configure('views', {
     autoescape: true,
